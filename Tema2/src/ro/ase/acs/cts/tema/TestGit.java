32fbba67ed0436e0c6cf9fc2a9aa58265c44c618
teodo <teodo@LAPTOP-0JSFIQAK.home>
2020-03-23 17:28:38 +0200

    commit initial cu clasa TestGit

---
+++ Tema2/src/ro/ase/acs/cts/tema/TestGit.java
@@ -0,0 +1,10 @@
+package ro.ase.acs.cts.tema;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licență este Aplicatie mobila pentru trasee montane!");
+
+	}
+
+}
